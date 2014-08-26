5145f14d72f0ded2e44265a8feb35c5a8907434d
Guto Oliveira <luizaugusto.contato@gmail.com>
2014-08-26 19:55:20 -0300

    Aulas Handler, Request e Response.

---
+++ tekton/backend/appengine/routes/rota/exemplo.py
@@ -0,0 +1,19 @@
+__author__ = 'GutoOliveira'
+# -*- coding: utf-8 -*-
+from gaecookie.decorator import no_csrf
+from gaepermission.decorator import login_not_required
+from tekton import router
+
+
+@login_not_required
+@no_csrf
+def index(_handler):
+    path = router.to_path(func)
+    _handler.redirect(path)
+
+
+@login_not_required
+@no_csrf
+def func(_resp, _req):
+    nome = _req.get('Vai...')
+    _resp.write('%s' % (nome))
---
+++ tekton/backend/appengine/routes/rota/home.py
@@ -0,0 +1,12 @@
+__author__ = 'GutoOliveira'
+
+# -*- coding: utf-8 -*-
+
+from gaecookie.decorator import no_csrf
+from gaepermission.decorator import login_not_required
+
+
+@login_not_required
+@no_csrf
+def index(_resp):
+    _resp.write("Rota")
