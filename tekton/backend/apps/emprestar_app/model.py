22dbfe30e7b41b10594e3d6b3aadb9cdc36a6677
Guto Oliveira <luizaugusto.contato@gmail.com>
2014-08-26 23:24:36 -0300

    tentativa de criação das apps

---
+++ tekton/backend/apps/emprestar_app/model.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import absolute_import, unicode_literals
+from google.appengine.ext import ndb
+from gaegraph.model import Node
+from gaeforms.ndb import property
+
+
+class Emprestar(Node):
+    pertence = ndb.StringProperty(required=True)
+    nome = ndb.StringProperty(required=True)
+    descricao = ndb.StringProperty(required=True)
+    emprestar = ndb.DateProperty(required=True)
+
