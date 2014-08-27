6c55df99c49428186394a8998b5b7dd4c9676868
Guto Oliveira <luizaugusto.contato@gmail.com>
2014-08-26 22:20:43 -0300

    Conseguindo fazer uso de templates jinja2

--- tekton/backend/appengine/routes/rota/home.py
+++ tekton/backend/appengine/routes/rota/home.py
@@ -1,3 +1,4 @@
+# coding=utf-8
 from config.template_middleware import TemplateResponse
 
 __author__ = 'GutoOliveira'
@@ -10,6 +11,41 @@ from gaepermission.decorator import login_not_required
 
 @login_not_required
 @no_csrf
-def index(nome='Guto',sobrenome="Oliveira"):
-    contexto = {'name': nome, 'lastname': sobrenome}
+def index():
+    class Pertence(object):
+        def __init__(self, imagem, nome, desc):
+            self.imagem = imagem
+            self.nome = nome
+            self.desc = desc
+
+    pertences = [
+        Pertence('/static/img/product/canon.jpg', 'Camera fotografica', 'CANON EOS 40D'),
+        Pertence('/static/img/product/canon.jpg', 'Camera fotografica', 'CANON EOS 40D'),
+        Pertence('/static/img/product/canon.jpg', 'Camera fotografica', 'CANON EOS 40D'),
+        Pertence('/static/img/product/canon.jpg', 'Camera fotografica', 'CANON EOS 40D'),
+        Pertence('/static/img/product/canon.jpg', 'Camera fotografica', 'CANON EOS 40D'),
+        Pertence('/static/img/product/canon.jpg', 'Camera fotografica', 'CANON EOS 40D'),
+        Pertence('/static/img/product/canon.jpg', 'Camera fotografica', 'CANON EOS 40D'),
+        Pertence('/static/img/product/canon.jpg', 'Camera fotografica', 'CANON EOS 40D'),
+        Pertence('/static/img/product/canon.jpg', 'Camera fotografica', 'CANON EOS 40D'),
+        Pertence('/static/img/product/canon.jpg', 'Camera fotografica', 'CANON EOS 40D'),
+        Pertence('/static/img/product/canon.jpg', 'Camera fotografica', 'CANON EOS 40D'),
+        Pertence('/static/img/product/canon.jpg', 'Camera fotografica', 'CANON EOS 40D'),
+        Pertence('/static/img/product/canon.jpg', 'Camera fotografica', 'CANON EOS 40D'),
+        Pertence('/static/img/product/canon.jpg', 'Camera fotografica', 'CANON EOS 40D'),
+        Pertence('/static/img/product/canon.jpg', 'Camera fotografica', 'CANON EOS 40D'),
+        Pertence('/static/img/product/canon.jpg', 'Camera fotografica', 'CANON EOS 40D'),
+        Pertence('/static/img/product/canon.jpg', 'Camera fotografica', 'CANON EOS 40D'),
+        Pertence('/static/img/product/canon.jpg', 'Camera fotografica', 'CANON EOS 40D'),
+        Pertence('/static/img/product/canon.jpg', 'Camera fotografica', 'CANON EOS 40D'),
+        Pertence('/static/img/product/canon.jpg', 'Camera fotografica', 'CANON EOS 40D'),
+        Pertence('/static/img/product/canon.jpg', 'Camera fotografica', 'CANON EOS 40D'),
+        Pertence('/static/img/product/canon.jpg', 'Camera fotografica', 'CANON EOS 40D'),
+        Pertence('/static/img/product/canon.jpg', 'Camera fotografica', 'CANON EOS 40D'),
+        Pertence('/static/img/product/canon.jpg', 'Camera fotografica', 'CANON EOS 40D'),
+        Pertence('/static/img/product/canon.jpg', 'Camera fotografica', 'CANON EOS 40D'),
+        Pertence('/static/img/product/canon.jpg', 'Camera fotografica', 'CANON EOS 40D')
+    ]
+
+    contexto = {'pertences': pertences}
     return TemplateResponse(contexto)
