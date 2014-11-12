f5509776f87d2ab306d578e94ec0cc7662dd037f
Guto Oliveira <luizaugusto.contato@gmail.com>
2014-11-12 17:51:07 -0200

    isolando rest api emprestarApi

--- tekton/backend/appengine/static/emprestar/js/emprestar.js
+++ tekton/backend/appengine/static/emprestar/js/emprestar.js
@@ -6,27 +6,54 @@ app.controller("EmprestarCtrl", function($scope){
     }
 });
 
+app.factory('emprestarApi', function($http){
+
+    var url_salvar_pertence = "/emprestars/rest/save";
+
+    var salvar_pertence = function(pertence, nome, descricao, emprestar){
+        var data = {
+            pertence: pertence,
+            nome: nome,
+            descricao: descricao,
+            emprestar: emprestar
+        };
+
+        return $http.post(
+            url_salvar_pertence,
+            data
+        );
+    };
+
+    var deletar_pertence = function(id_pertence){
+
+    };
+
+    return {
+        salvar_pertence: salvar_pertence,
+        deletar_pertence: deletar_pertence
+    }
+});
+
 app.directive("emprestarform", function($http){
    return{
        restrict: "E",
-       transclude: true,
+       replace: true,
        templateUrl: "/static/emprestar/html/form.html",
        scope:{
          toggleshow: "&",
          elementos: "="
        },
-       link: function($scope, elm, attrs){
+       controller: function($scope, emprestarApi){
 
            $scope.salvando = false;
            $scope.salvar = function() {
                $scope.salvando = true;
-               var data = {pertence: $scope.pertence, nome: $scope.nome, descricao: $scope.descricao, emprestar: $scope.emprestar};
-               $http.post("/emprestars/rest/save", data).success( function(result){
+               emprestarApi.salvar_pertence($scope.pertence, $scope.nome, $scope.descricao,
+                   $scope.emprestar).success( function(result){
                     $scope.elementos.push(result)
                }).finally(function(){
                    $scope.salvando = false;
                });
-
            };
        }
    };
